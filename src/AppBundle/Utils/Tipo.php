101fda5961ec5bad9e096ad0694b03dcd0496840
Rafael Vilarino <rovilarino@gmail.com>
2017-04-17 23:04:20 -0300

    Extração regiões

---
+++ src/AppBundle/Utils/Tipo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafael
+ * Date: 17/04/17
+ * Time: 21:12
+ */
+
+namespace AppBundle\Utils;
+
+
+class Tipo
+{
+
+    const DADOS = 1;
+    const DICIONARIO = 2;
+}
